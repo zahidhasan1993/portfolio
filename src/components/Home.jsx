75dcd2edd2473782c0ba871e8976e2b61c6b3be9
devZahid <officialzahidhasan19932021@gmail.com>
2023-06-21 13:32:48 +0600

    home component complete

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,11 +1,33 @@
-import React from 'react';
+import React from "react";
+import { HiArrowNarrowRight } from "react-icons/hi";
 
 const Home = () => {
-    return (
-        <div className='w-full h-screen bg-[#0a192f]'>
-            
+  return (
+    <div className="w-full h-screen bg-[#0a192f]">
+      <div className="max-w-[1000px] mx-auto px-8 flex flex-col justify-center h-full">
+        <p className="text-white text-xl">Hi,my name is</p>
+        <h1 className="text-4xl sm:text-7xl font-bold text-[#ccd6f6]">
+          Zahid Hasan
+        </h1>
+        <h2 className="text-4xl sm-text-7xl font-bold text-[#8892b0]">
+          I'm a MERN Stack Developer
+        </h2>
+        <p className="text-[#8892b0] py-4 max-w-[700px]">
+          I'm a full-stack developer specializing in building (and occasionally
+          designing) exceptional digital experiences. CUrrently, i'm focused on
+          building responsive full-stack web applications.
+        </p>
+        <div>
+          <button className="text-white group border-2 px-6 py-3 my-2 flex gap-2 items-center hover:bg-white hover:text-black">
+            View Projects
+            <span className="group-hover:rotate-90 duration-500">
+              <HiArrowNarrowRight></HiArrowNarrowRight>
+            </span>
+          </button>
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 export default Home;
