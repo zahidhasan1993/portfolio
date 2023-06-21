96ef8093d55b672970dbc267abff7f369f220df6
devZahid <officialzahidhasan19932021@gmail.com>
2023-06-21 13:56:16 +0600

    about sections complete

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -3,7 +3,7 @@ import { HiArrowNarrowRight } from "react-icons/hi";
 
 const Home = () => {
   return (
-    <div className="w-full h-screen bg-[#0a192f]">
+    <div name='home' className="w-full h-screen bg-[#0a192f]">
       <div className="max-w-[1000px] mx-auto px-8 flex flex-col justify-center h-full">
         <p className="text-white text-xl">Hi,my name is</p>
         <h1 className="text-4xl sm:text-7xl font-bold text-[#ccd6f6]">
