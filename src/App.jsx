d7a6c6cab2889f7c2e7606a27034d141dcc72018
devZahid <officialzahidhasan19932021@gmail.com>
2023-07-02 13:28:16 +0600

    adding frammer motion

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
-import React from "react";
+import React, { useEffect } from "react";
+import "aos/dist/aos.css";
 import Navbar from "./components/Navbar";
 import Home from "./components/Home";
 import About from "./components/About";
@@ -7,11 +8,14 @@ import Projects from "./components/Projects";
 import Contact from "./components/Contact";
 import Footer from "./components/Footer";
 
+
 const App = () => {
   return (
     <div className="bg-[#0a192f]">
       <Navbar></Navbar>
+
       <Home></Home>
+
       <About></About>
       <Skills></Skills>
       <Projects></Projects>
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,9 +1,10 @@
 import React from "react";
 import { HiArrowNarrowRight } from "react-icons/hi";
+import { motion } from "framer-motion";
 
 const Home = () => {
   return (
-    <div name='home' className="w-full pt-40 md:pt-0 md:h-screen bg-[#0a192f]">
+    <div name="home" className="w-full pt-40 md:pt-0 md:h-screen bg-[#0a192f]">
       <div className="max-w-[1000px] mx-auto px-8 flex flex-col justify-center h-full">
         <p className="text-white text-xl">Hi,my name is</p>
         <h1 className="text-4xl sm:text-7xl font-bold text-[#ccd6f6]">
@@ -12,20 +13,31 @@ const Home = () => {
         <h2 className="text-4xl sm-text-7xl font-bold text-[#8892b0]">
           I'm a MERN Stack Developer
         </h2>
+
         <p className="text-[#8892b0] py-4 max-w-[700px]">
           I'm a full-stack developer specializing in building (and occasionally
           designing) exceptional digital experiences. CUrrently, i'm focused on
           building responsive full-stack web applications.
         </p>
+
         <div>
-          <a href="https://drive.google.com/file/d/1EOOOAgQ4vYU0LZnw1FPWpKazYAwIq9PS/view?usp=sharing">
-          <button className="text-white group border-2 px-6 py-3 my-2 flex gap-2 items-center hover:bg-white hover:text-black">
-            Download Resume
-            <span className="group-hover:rotate-90 duration-500">
-              <HiArrowNarrowRight></HiArrowNarrowRight>
-            </span>
-          </button>
-          </a>
+          <motion.div
+            initial={{ x: -30 }}
+            animate={{ x: 30 }}
+            transition={{ duration: 10, repeat: Infinity, repeatType: "mirror" }}
+            style={{
+              position: "relative",
+            }}
+          >
+            <a href="https://drive.google.com/file/d/1EOOOAgQ4vYU0LZnw1FPWpKazYAwIq9PS/view?usp=sharing">
+              <button className="text-white group border-2 px-6 py-3 my-2 flex gap-2 items-center hover:bg-white hover:text-black">
+                Download Resume
+                <span className="group-hover:rotate-90 duration-500">
+                  <HiArrowNarrowRight></HiArrowNarrowRight>
+                </span>
+              </button>
+            </a>
+          </motion.div>
         </div>
       </div>
     </div>
