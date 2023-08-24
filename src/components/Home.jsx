fd75fe38b827a7112284d3e8a31eb36ce750d43b
devZahid <officialzahidhasan19932021@gmail.com>
2023-08-24 15:40:07 +0600

    some changes

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,24 +1,22 @@
 import React from "react";
 import { HiArrowNarrowRight } from "react-icons/hi";
 import { motion } from "framer-motion";
-import "./style.css"
+import "./style.css";
 const Home = () => {
   return (
     <div name="home" className="w-full pt-40 md:pt-0 md:h-screen bg-[#0a192f]">
       <div className="max-w-[1000px] mx-auto px-8 flex flex-col justify-center h-full">
         <p className="text-white text-xl">Hi,my name is</p>
-        {/* <h1 className="text-4xl sm:text-7xl font-bold text-[#ccd6f6]">
+
+        <motion.p
+          className="text-7xl"
+          animate={{
+            color: ["#BEE3F8", "#93C5FD", "#BEE3F8"],
+            transition: { duration: 1, repeat: Infinity },
+          }}
+        >
           Zahid Hasan
-        </h1> */}
-         <motion.p
-        className="text-7xl"
-        animate={{
-          color: ['#BEE3F8', '#93C5FD', '#BEE3F8'],
-          transition: { duration: 1, repeat: Infinity },
-        }}
-      >
-        Zahid Hasan
-      </motion.p>
+        </motion.p>
         <h2 className="text-4xl sm-text-7xl font-bold text-[#8892b0]">
           I'm a MERN Stack Developer
         </h2>
@@ -30,15 +28,8 @@ const Home = () => {
         </p>
 
         <div>
-          <motion.div
-            initial={{ x: -30 }}
-            animate={{ x: 30 }}
-            transition={{ duration: 5, repeat: Infinity, repeatType: "mirror" }}
-            style={{
-              position: "relative",
-            }}
-          >
-            <a href="https://drive.google.com/file/d/1EOOOAgQ4vYU0LZnw1FPWpKazYAwIq9PS/view?usp=sharing">
+          <div>
+            <a href="https://drive.google.com/file/d/1m8kPUhYghx-WVr51aeqTx1zo8ReBdaKN/view?usp=sharing">
               <button className="text-white group border-2 px-6 py-3 my-2 flex gap-2 items-center hover:bg-white hover:text-black">
                 Download Resume
                 <span className="group-hover:rotate-90 duration-500">
@@ -46,7 +37,7 @@ const Home = () => {
                 </span>
               </button>
             </a>
-          </motion.div>
+          </div>
         </div>
       </div>
     </div>
--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -30,173 +30,142 @@ const Projects = () => {
           <p className="py-12">// These are the projects I've worked with</p>
         </div>
         <div className="md:grid md:grid-cols-3 md:gap-10">
-          <motion.div
-            whileHover={{ scale: 1.3 }}
-            transition={{ duration: 0.3 }}
-            style={{
-              position: "relative",
-            }}
-          >
-            <div className="bg-none text-white shadow-lg rounded-lg overflow-hidden w-full">
-              <img className="w-52 ml-12" src={project1} alt="Card" />
-              <div className="p-4">
-                <h1 className="text-xs pb-3">
-                  &#128178; Full MERN Stack Project &#128640;
-                </h1>
-                <h1 className="font-bold my-2">
-                  &#127927; Rhythm-Retreat &#127928;
-                </h1>
-                <p className="mb-2">
-                  This project is based on a summer camp school.This app is have
-                  3 different dashboard for 3 different users.Including a
-                  payment method and many more...
-                </p>
+          <div className="bg-none text-white shadow-lg rounded-lg overflow-hidden w-full hover:scale-125 transition-transform">
+            <img className="w-52 ml-12" src={project1} alt="Card" />
+            <div className="p-4">
+              <h1 className="text-xs pb-3">
+                &#128178; Full MERN Stack Project &#128640;
+              </h1>
+              <h1 className="font-bold my-2">
+                &#127927; Rhythm-Retreat &#127928;
+              </h1>
+              <p className="mb-2">
+                This project is based on a summer camp school.This app is have 3
+                different dashboard for 3 different users.Including a payment
+                method and many more...
+              </p>
 
-                <div className="flex justify-between mt-8">
-                  <div className="flex justify-center items-center gap-2">
-                    <button className="text-White">
-                      <a href="https://github.com/zahidhasan1993/rhythm-retreat-clint">
-                        Github
-                      </a>
-                    </button>
-                    <FaArrowRight />
-                  </div>
-                  <div className="flex justify-center items-center gap-2 text-blue-500">
-                    <button className="">
-                      <a href="https://rhythm-retreate.web.app/">Live site</a>
-                    </button>
-                    <FaArrowRight />
-                  </div>
+              <div className="flex justify-between mt-8">
+                <div className="flex justify-center items-center gap-2">
+                  <button className="text-White">
+                    <a href="https://github.com/zahidhasan1993/rhythm-retreat-clint">
+                      Github
+                    </a>
+                  </button>
+                  <FaArrowRight />
+                </div>
+                <div className="flex justify-center items-center gap-2 text-blue-500">
+                  <button className="">
+                    <a href="https://rhythm-retreate.web.app/">Live site</a>
+                  </button>
+                  <FaArrowRight />
                 </div>
               </div>
             </div>
-          </motion.div>
-          <motion.div
-            whileHover={{ scale: 1.3 }}
-            transition={{ duration: 0.3 }}
-            style={{
-              position: "relative",
-            }}
-          >
-            <div className="bg-none text-white shadow-lg rounded-lg overflow-hidden w-full">
-              <img className="w-52 ml-12" src={project2} alt="Card" />
-              <div className="p-4">
-                <h1 className="text-xs pb-3">
-                  &#128178; Full MERN Stack Project &#128640;
-                </h1>
-                <h1 className="font-bold my-2">
-                  &#129369; DesiCuisine &#129367;
-                </h1>
+          </div>
 
-                <p className="mb-2">
-                  This Website is based on a food website theme. Here user can
-                  authenticate and login they can see their favorite chef's
-                  recipes.
-                </p>
+          <div className="bg-none text-white shadow-lg rounded-lg overflow-hidden w-full hover:scale-125 transition-transform">
+            <img className="w-52 ml-12" src={project2} alt="Card" />
+            <div className="p-4">
+              <h1 className="text-xs pb-3">
+                &#128178; Full MERN Stack Project &#128640;
+              </h1>
+              <h1 className="font-bold my-2">
+                &#129369; DesiCuisine &#129367;
+              </h1>
 
-                <div className="flex justify-between mt-8">
-                  <div className="flex justify-center items-center gap-2">
-                    <button className="text-White">
-                      <a href="https://github.com/zahidhasan1993/desiCuisine">
-                        Github
-                      </a>
-                    </button>
-                    <FaArrowRight />
-                  </div>
-                  <div className="flex justify-center items-center gap-2 text-blue-500">
-                    <button className="">
-                      <a href="https://desicuisin-8119b.web.app/">Live site</a>
-                    </button>
-                    <FaArrowRight />
-                  </div>
+              <p className="mb-2">
+                This Website is based on a food website theme. Here user can
+                authenticate and login they can see their favorite chef's
+                recipes.
+              </p>
+
+              <div className="flex justify-between mt-8">
+                <div className="flex justify-center items-center gap-2">
+                  <button className="text-White">
+                    <a href="https://github.com/zahidhasan1993/desiCuisine">
+                      Github
+                    </a>
+                  </button>
+                  <FaArrowRight />
+                </div>
+                <div className="flex justify-center items-center gap-2 text-blue-500">
+                  <button className="">
+                    <a href="https://desicuisin-8119b.web.app/">Live site</a>
+                  </button>
+                  <FaArrowRight />
                 </div>
               </div>
             </div>
-          </motion.div>
-          <motion.div
-            whileHover={{ scale: 1.3 }}
-            transition={{ duration: 0.3 }}
-            style={{
-              position: "relative",
-            }}
-          >
-            <div className="bg-none text-white shadow-lg rounded-lg overflow-hidden w-full">
-              <img className="w-52 ml-12 mt-16" src={project3} alt="Card" />
-              <div className="p-4 mt-10">
-                <h1 className="text-xs pb-3">
-                  &#128178; Full MERN Stack Project &#128640;
-                </h1>
-                <h1 className="font-bold my-2">
-                  &#128667; WhizzyWheels &#128663;
-                </h1>
+          </div>
 
-                <p className="mb-2">
-                  This app is a toy car selling website. User after registration
-                  can add their toys on the app and sell them. They can update
-                  or delete a toy and many more...
-                </p>
+          <div className="bg-none text-white shadow-lg rounded-lg overflow-hidden w-full hover:scale-125 transition-transform">
+            <img className="w-52 ml-12 mt-16" src={project3} alt="Card" />
+            <div className="p-4 mt-10">
+              <h1 className="text-xs pb-3">
+                &#128178; Full MERN Stack Project &#128640;
+              </h1>
+              <h1 className="font-bold my-2">
+                &#128667; WhizzyWheels &#128663;
+              </h1>
 
-                <div className="flex justify-between mt-8">
-                  <div className="flex justify-center items-center gap-2">
-                    <button className="text-White">
-                      <a href="https://github.com/zahidhasan1993/whizzyWheels">
-                        Github
-                      </a>
-                    </button>
-                    <FaArrowRight />
-                  </div>
-                  <div className="flex justify-center items-center gap-2 text-blue-500">
-                    <button className="">
-                      <a href="https://whizzywheels.web.app/">Live site</a>
-                    </button>
-                    <FaArrowRight />
-                  </div>
+              <p className="mb-2">
+                This app is a toy car selling website. User after registration
+                can add their toys on the app and sell them. They can update or
+                delete a toy and many more...
+              </p>
+
+              <div className="flex justify-between mt-8">
+                <div className="flex justify-center items-center gap-2">
+                  <button className="text-White">
+                    <a href="https://github.com/zahidhasan1993/whizzyWheels">
+                      Github
+                    </a>
+                  </button>
+                  <FaArrowRight />
+                </div>
+                <div className="flex justify-center items-center gap-2 text-blue-500">
+                  <button className="">
+                    <a href="https://whizzywheels.web.app/">Live site</a>
+                  </button>
+                  <FaArrowRight />
                 </div>
               </div>
             </div>
-          </motion.div>
-          <motion.div
-            whileHover={{ scale: 1.3 }}
-            transition={{ duration: 0.3 }}
-            style={{
-              position: "relative",
-            }}
-          >
-            <div className="bg-none text-white shadow-lg rounded-lg overflow-hidden w-full">
-              <img className="w-52 ml-12" src={project4} alt="Card" />
-              <div className="p-4">
-                <h1 className="text-xs pb-3">
-                  &#128178; Full MERN Stack Project &#128640;
-                </h1>
-                <h1 className="font-bold my-2">
-                  &#128667; AutoMedics &#128668;
-                </h1>
+          </div>
 
-                <p className="mb-2">
-                  On this app a user can choose what he want to repair in his
-                  cars. This app is based on car repair website. User can book
-                  their time and many more....
-                </p>
+          <div className="bg-none text-white shadow-lg rounded-lg overflow-hidden w-full hover:scale-125 transition-transform">
+            <img className="w-52 ml-12" src={project4} alt="Card" />
+            <div className="p-4">
+              <h1 className="text-xs pb-3">
+                &#128178; Full MERN Stack Project &#128640;
+              </h1>
+              <h1 className="font-bold my-2">&#128667; AutoMedics &#128668;</h1>
 
-                <div className="flex justify-between mt-8">
-                  <div className="flex justify-center items-center gap-2">
-                    <button className="text-White">
-                      <a href="https://github.com/zahidhasan1993/AutoMedics-clint">
-                        Github{" "}
-                      </a>
-                    </button>
-                    <FaArrowRight />
-                  </div>
-                  <div className="flex justify-center items-center gap-2 text-blue-500">
-                    <button className="">
-                      <a href="https://automedics-48c9f.web.app/">Live site</a>
-                    </button>
-                    <FaArrowRight />
-                  </div>
+              <p className="mb-2">
+                On this app a user can choose what he want to repair in his
+                cars. This app is based on car repair website. User can book
+                their time and many more....
+              </p>
+
+              <div className="flex justify-between mt-8">
+                <div className="flex justify-center items-center gap-2">
+                  <button className="text-White">
+                    <a href="https://github.com/zahidhasan1993/AutoMedics-clint">
+                      Github{" "}
+                    </a>
+                  </button>
+                  <FaArrowRight />
+                </div>
+                <div className="flex justify-center items-center gap-2 text-blue-500">
+                  <button className="">
+                    <a href="https://automedics-48c9f.web.app/">Live site</a>
+                  </button>
+                  <FaArrowRight />
                 </div>
               </div>
             </div>
-          </motion.div>
+          </div>
         </div>
       </div>
     </div>
