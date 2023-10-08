9d263277e8985810468b278b67e9462bee47dfd1
devZahid <officialzahidhasan19932021@gmail.com>
2023-10-09 00:52:27 +0600

    new project added

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,6 +1,5 @@
 import React from "react";
 import project1 from "../assets/Images/projects/1.png";
-import project2 from "../assets/Images/projects/2.png";
 import project3 from "../assets/Images/projects/3.png";
 import project4 from "../assets/Images/projects/4.png";
 import { FaArrowRight } from "react-icons/fa";
@@ -30,6 +29,39 @@ const Projects = () => {
           <p className="py-12">// These are the projects I've worked with</p>
         </div>
         <div className="md:grid md:grid-cols-3 md:gap-10">
+          <div className="bg-none text-white shadow-lg rounded-lg overflow-hidden w-full md:hover:scale-125 transition-transform">
+            <img className="w-52 ml-12" src={project4} alt="Card" />
+            <div className="p-4">
+              <h1 className="text-xs pb-3">
+                &#128178; Full MERN Stack Project &#128640;
+              </h1>
+              <h1 className="font-bold my-2">&#128667; Dineease &#128668;</h1>
+
+              <p className="mb-2">
+                This App is a restaurant management app. This app is full build.
+                user can order and also payment for their food, they can book
+                their tables etc. Admin also can put new food in the list.
+                Approve bookings can make user admin and many more....
+              </p>
+
+              <div className="flex justify-between mt-8">
+                <div className="flex justify-center items-center gap-2">
+                  <button className="text-White">
+                    <a href="https://github.com/zahidhasan1993/dineease-clint">
+                      Github{" "}
+                    </a>
+                  </button>
+                  <FaArrowRight />
+                </div>
+                <div className="flex justify-center items-center gap-2 text-blue-500">
+                  <button className="">
+                    <a href="https://dineease-15e58.web.app/">Live site</a>
+                  </button>
+                  <FaArrowRight />
+                </div>
+              </div>
+            </div>
+          </div>
           <div className="bg-none text-white shadow-lg rounded-lg overflow-hidden w-full md:hover:scale-125 transition-transform">
             <img className="w-52 ml-12" src={project1} alt="Card" />
             <div className="p-4">
@@ -92,39 +124,6 @@ const Projects = () => {
                 <div className="flex justify-center items-center gap-2 text-blue-500">
                   <button className="">
                     <a href="https://whizzywheels.web.app/">Live site</a>
-                  </button>
-                  <FaArrowRight />
-                </div>
-              </div>
-            </div>
-          </div>
-
-          <div className="bg-none text-white shadow-lg rounded-lg overflow-hidden w-full md:hover:scale-125 transition-transform">
-            <img className="w-52 ml-12" src={project4} alt="Card" />
-            <div className="p-4">
-              <h1 className="text-xs pb-3">
-                &#128178; Full MERN Stack Project &#128640;
-              </h1>
-              <h1 className="font-bold my-2">&#128667; AutoMedics &#128668;</h1>
-
-              <p className="mb-2">
-                On this app a user can choose what he want to repair in his
-                cars. This app is based on car repair website. User can book
-                their time and many more....
-              </p>
-
-              <div className="flex justify-between mt-8">
-                <div className="flex justify-center items-center gap-2">
-                  <button className="text-White">
-                    <a href="https://github.com/zahidhasan1993/AutoMedics-clint">
-                      Github{" "}
-                    </a>
-                  </button>
-                  <FaArrowRight />
-                </div>
-                <div className="flex justify-center items-center gap-2 text-blue-500">
-                  <button className="">
-                    <a href="https://automedics-48c9f.web.app/">Live site</a>
                   </button>
                   <FaArrowRight />
                 </div>
